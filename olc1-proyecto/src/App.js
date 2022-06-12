4af859dc9c2918b1a419a2562f54e44acb1f7853
yeinnymelissa <3017261750101@ingenieria.usac.edu.gt>
2022-06-11 22:10:29 -0600

    Front

--- olc1-proyecto/src/App.js
+++ olc1-proyecto/src/App.js
@@ -4,12 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Superior from './components/Superior';
 import Botones from './components/Botones';
 import EditorCompleto from './components/EditorCompleto';
+import Editor from './components/Editor';
 
 function App() {
   return <BrowserRouter>
     <Superior />
-    <Botones />
-    <EditorCompleto />
+    <Editor />
   </BrowserRouter>
 }
 
--- olc1-proyecto/src/components/Botones.js
+++ olc1-proyecto/src/components/Botones.js
@@ -1,7 +1,19 @@
+import CodeMirror from '@uiw/react-codemirror';
+
 function Botones() {
+    function abrir(){
+        /*const li_ = document.querySelectorAll('.li');
+
+        li_.forEach((cadaLi, i)=>{
+            console.log(li_[i])
+        });*/
+        const prueba = document.querySelector("#lps");
+        
+    }
+
     return (
         <div className="botones">
-            <button type="button" className="btn btn-primary izq">Abrir</button>
+            <button type="button" className="btn btn-primary izq" onClick={abrir}>Abrir</button>
             <div className="dropdown izq">
                 <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
                     Guardar
